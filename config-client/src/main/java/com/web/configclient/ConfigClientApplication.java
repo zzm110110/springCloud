817320e02a5e38aca380cedacf4849f63a96da7d
zzmedu <1596762965@qq.com>
2018-10-10 11:40:33 +0800

    分布式配置中心客户端

---
+++ config-client/src/main/java/com/web/configclient/ConfigClientApplication.java
@@ -0,0 +1,17 @@
+package com.web.configclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @version <p>V1.0</p>
+ * @Author: <H2>james</H2>
+ * @Description: <P>添加说明</P>
+ * @Date: <P>CREATE IS 2018/10/10 11:00</P>
+ **/
+@SpringBootApplication
+public class ConfigClientApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientApplication.class, args);
+    }
+}
