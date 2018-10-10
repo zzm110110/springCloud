b6732027435fc119f0fd6bbc1a1f791b8b5a1e54
zzmedu <1596762965@qq.com>
2018-10-10 16:31:24 +0800

    消息总线

---
+++ service-amqp/src/main/java/com/web/serviceamqp/controller/TestController.java
@@ -0,0 +1,24 @@
+package com.web.serviceamqp.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @version <p>V1.0</p>
+ * @Author: <H2>james</H2>
+ * @Description: <P>添加说明</P>
+ * @Date: <P>CREATE IS 2018/10/10 16:22</P>
+ **/
+@RestController
+public class TestController {
+
+    @Value("${foo}")
+    String foo;
+
+    @RequestMapping(value = "/hi")
+    public String hi(){
+        return foo;
+    }
+
+}
