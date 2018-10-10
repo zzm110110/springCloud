d8dec680127e7e3587930ff58071818233bf3d3a
zzmedu <1596762965@qq.com>
2018-10-10 14:46:24 +0800

    分布式配置中心服务端

--- config-server/src/main/java/com/web/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/com/web/configserver/ConfigServerApplication.java
@@ -3,6 +3,7 @@ package com.web.configserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @version <p>V1.0</p>
