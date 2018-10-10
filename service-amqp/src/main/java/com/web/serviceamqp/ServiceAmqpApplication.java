fb064c784d437e69d347bc95bebb48347c74f1d6
zzmedu <1596762965@qq.com>
2018-10-10 16:04:44 +0800

    消息总线

---
+++ service-amqp/src/main/java/com/web/serviceamqp/ServiceAmqpApplication.java
@@ -0,0 +1,12 @@
+package com.web.serviceamqp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceAmqpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceAmqpApplication.class, args);
+    }
+}
